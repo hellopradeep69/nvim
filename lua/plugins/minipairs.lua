2c47d95c22d7b0109daea103575c1340929d2d04
k.pradeep <Hellopradeep69@gmail.com>
2026-02-28 14:50:56 +0530

    mini pairs no <<>>

--- lua/plugins/minipairs.lua
+++ lua/plugins/minipairs.lua
@@ -1,10 +1,6 @@
 return {
 	"nvim-mini/mini.pairs",
 	config = function()
-		require("mini.pairs").setup({
-			mappings = {
-				["<"] = { action = "open", pair = "<>", neigh_pattern = "[^\\]." },
-			},
-		})
+		require("mini.pairs").setup()
 	end,
 }
