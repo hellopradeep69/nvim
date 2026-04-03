3a2d8451d6ea5ea17b2401b85c57b87e9bddd359
k.pradeep <Hellopradeep69@gmail.com>
2026-04-03 20:09:12 +0530

    updated blink and keymap

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -25,6 +25,7 @@ return {
 
 			sources = {
 				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+				-- default = { "lsp", "path", "snippets", "buffer" },
 				providers = {
 					lazydev = {
 						name = "LazyDev",
