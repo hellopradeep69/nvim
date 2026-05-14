357db4f697f57d703a21c559f93ffe51110ef7c7
k.pradeep <Hellopradeep69@gmail.com>
2026-05-14 15:04:11 +0530

    Colorscheme config

--- plugin/colorscheme.lua
+++ plugin/colorscheme.lua
@@ -2,6 +2,13 @@ require("gruvbox").setup({
 	styles = {
 		transparency = true,
 	},
+	highlight_groups = {
+		LspReferenceRead = { bg = "bg2", underline = false },
+		LspReferenceText = { bg = "bg_second", underline = false },
+		LspReferenceWrite = { bg = "bg_second", underline = false },
+		LspSignatureActiveParameter = { fg = "bg_main", bg = "green_lite", underline = false },
+		LspReferenceTarget = { fg = "fg1", bg = "bg_second", underline = false },
+	},
 })
 
 vim.cmd("colorscheme gruvbox")
